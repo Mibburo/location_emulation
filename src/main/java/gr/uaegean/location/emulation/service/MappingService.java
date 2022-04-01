40c53cd1686fdce25ea094c820ac53cf5364153a
Kon <c.bitsikas@gmail.com>
2022-04-01 21:26:19 +0300

    fix for resource file

--- src/main/java/gr/uaegean/location/emulation/service/MappingService.java
+++ src/main/java/gr/uaegean/location/emulation/service/MappingService.java
@@ -7,15 +7,10 @@ import org.springframework.cache.annotation.Cacheable;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PostConstruct;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.io.*;
 
 @Slf4j
 @Component
@@ -44,43 +39,12 @@ public class MappingService {
         return imageArray;
     }*/
 
-    /*@Cacheable("imageMap")
-    public String[][] convertImageToColorArray(EmulationDTO dto) throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("static/images/palaemon_ship_diagram_with_geofences.png").getFile()); // Be sure to read input file, you have error reading it.
-
-        if(dto.getImageFile() != null && !"".equals(dto.getImageFile().getOriginalFilename().trim())){
-            dto.getImageFile().transferTo(file);
-        }
-        log.info("aaaaaaaaaaaaaaaaaaaaa cache ");
-
-        BufferedImage bufferedImage = ImageIO.read(file);
-
-        String[][] imageArray = new String[bufferedImage.getWidth()][bufferedImage.getHeight()];
-        for (int i = 0; i < bufferedImage.getWidth(); i++) {
-            for (int j = 0; j < bufferedImage.getHeight(); j++) {
-                //int pixel = wr.getSample(i, j, 0); // the sample in the specified band for the pixel at the specified coordinate.
-                Color c = new Color(bufferedImage.getRGB(i,j));
-                int red = c.getRed();
-                int green = c.getGreen();
-                int blue = c.getBlue();
-                String hex = String.format("#%02X%02X%02X", red, green, blue);
-                imageArray[i][j] = hex;
-            }
-        }
-
-        return imageArray;
-    }*/
-
     @EventListener(ApplicationReadyEvent.class)
     @Cacheable("imageMap")
     public String[][] convertImageToColorArray() throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("static/images/palaemon_ship_diagram_with_geofences.png").getFile()); // Be sure to read input file, you have error reading it.
 
-        log.info("cached map");
-
-        BufferedImage bufferedImage = ImageIO.read(file);
+        InputStream in = getClass().getClassLoader().getResourceAsStream("static/images/palaemon_ship_diagram_with_geofences.png");
+        BufferedImage bufferedImage = ImageIO.read(in);
 
         String[][] imageArray = new String[bufferedImage.getWidth()][bufferedImage.getHeight()];
         for (int i = 0; i < bufferedImage.getWidth(); i++) {
@@ -94,19 +58,16 @@ public class MappingService {
                 imageArray[i][j] = hex;
             }
         }
-
+        log.info("cached map");
         return imageArray;
     }
 
     //for visualization purposes
     public String[][] convertImageToInvertedArray(EmulationDTO dto) throws IOException {
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("static/images/palaemon_ship_diagram_with_geofences.png").getFile()); // Be sure to read input file, you have error reading it.
 
-        if(dto.getImageFile() != null){
-            dto.getImageFile().transferTo(file);
-        }
-        BufferedImage bufferedImage = ImageIO.read(file);
+        InputStream in = getClass().getResourceAsStream("static/images/palaemon_ship_diagram_with_geofences.png");
+
+        BufferedImage bufferedImage = ImageIO.read(in);
 
         String[][] imageArray = new String[bufferedImage.getHeight()][bufferedImage.getWidth()];
         for (int i = 0; i < bufferedImage.getHeight(); i++) {
